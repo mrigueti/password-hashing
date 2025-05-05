96d04702d11df2d306b2e135949598d0ea7b217e
mrigueti <maykelrigueti93@gmail.com>
2025-05-05 01:19:33 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func main() {
+	var password string
+	fmt.Print("Enter a password: ")
+	fmt.Scanln(&password)
+
+	hash, _ := HashPassword(password)
+
+    fmt.Println("Password:", password)
+    fmt.Println("Hash:    ", hash)
+
+    match := CheckPasswordHash(password, hash)
+    fmt.Println("Match:   ", match)
+}
